afed767882478659b97e81cce35c5f022535be2b
Minhee-Jang/장민희 <minhee_jang@ewha.ac.kr>
2025-04-08 16:16:45 +0900

    Title: 마법의 숲 탐색, Time: 56ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250408/마법의 숲 탐색/magical-forest-exploration.py
@@ -0,0 +1,110 @@
+from collections import deque
+def get_rowmax(forest, r, c, d):   #DFS
+    r, c = r - 2, c
+    row_max = max(r + gol[d][0], r + 1)  # 본인 위치에서 갈 수 있는 최대
+    visited = [[False] * (C+1) for _ in range(R-2)]
+    map_f = []
+    for i in range(2, R):  # R=9
+        map_f.append(forest[i])
+    for dr, dc in gol:
+        visited[r+dr][c+dc] = True #본인 골렘은 갈필요 x
+
+    myque = deque()
+    myque.append((r + gol[d][0], c+ gol[d][1]))  ## 출구위치에서 시작
+    while myque:  #idnex R, C로 해도 됨
+        start_r, start_c = myque.popleft()
+        for dr, dc in find_max:
+            nex_r, nex_c = start_r + dr, start_c + dc
+            if 0<nex_r<=R-3 and 0<nex_c<=C:
+                if visited[nex_r][nex_c] == False and map_f[nex_r][nex_c] !=0: # 갈수 있는 곳
+                    visited[nex_r][nex_c] = True
+                    row_max = max(row_max, nex_r)
+                    myque.append((nex_r, nex_c)) #다음 위치 탐색
+
+    return row_max
+
+def possible(direct, r, c):
+    direction = finds[direct]
+    for dr, dc in direction:  # 남쪽 탐색
+        fr, fc = r + dr, c + dc  # 이동할 좌표 위치 탐색
+        if 0<fr<R and 0<fc<=C: # 만약에 fr fc 이동 가능
+            if forest[fr][fc] !=0: # 이미 있음
+                return False
+        else: # index 밖 범위는 이동 불가
+            return False
+    return True
+
+def find_forest(forest, g_d, ci, answer): #숲이랑 골렘의 출구방향
+
+    r, c = 1, ci    #골렘 시작 위치 (중심)  - 골렘 0행부터 시작
+    while True:  #남쪽으로 계속 하강 먼저
+        if r == R - 2:  # 중심위치 최대면
+            for dr, dc in gol:
+                forest[r + dr][c + dc] = 1  # 위치저장
+            forest[r + gol[g_d][0]][c + gol[g_d][1]] = 2  # 출구위치 별도 저장
+            answer += get_rowmax(forest, r, c, g_d)
+            return forest, answer
+        else:
+            if possible(0, r, c): #남쪽 이동 가능
+                 r, c = r + 1, c
+
+            elif possible(1, r, c) and possible(0, r, c-1): #서쪽 + 남쪽 이동 가능
+                r, c = r +1 , c - 1 # 이동
+                if g_d == 0:
+                    g_d = 3
+                else:
+                    g_d = g_d - 1
+
+            elif possible(2, r, c) and possible(0, r, c+1): # 동쪽 + 남쪽 이동가능
+                r, c = r +1 , c + 1 # 동쪽 아래로 이동
+                g_d = (g_d + 1) % 4
+
+            else:  # 갈 곳 없으면
+                for dr, dc in gol:
+                    if r+dr<=2: #몸통 밖에 나가는지 확인
+                        forest = [[0 for _ in range(C + 1)] for _ in range(R)]
+                        return forest, answer
+
+
+                    forest[r+dr][c+dc] = 1 #위치저장
+                forest[r+gol[g_d][0]][c+gol[g_d][1]] = 2   #출구위치 별도 저장
+
+
+                #최대위치 조사 후 저장
+                answer += get_rowmax(forest, r, c, g_d)
+
+                return forest, answer
+
+def main():
+    global forest, answer
+
+
+    for c_i, di in com:
+        forest, answer = find_forest(forest, di, c_i, answer) # 숲 탐색
+
+
+
+if __name__=="__main__":
+    R, C, K = map(int, input().split())
+    R += 3
+    com= []
+    for _ in range(K):
+        d, c = map(int, input().split())
+        com.append((d, c))
+
+    gol = [(-1, 0), (0, 1), (1,0), (0,-1), (0,0)]   #골렘의 index 탐색하기 위한 북-동-남-서
+    forest = [[0 for _ in range(C+1)] for _ in range(R)]  #숲 -> 0 index로 출발 준비
+    find_south = [(1, -1), (2, 0), (1, 1)]
+    find_left =  [(-1, -1), (0, -2), (1, -1)]
+    find_right =  [(-1, 1), (0, 2), (1, 1)]
+    find_max = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+    finds = [find_south, find_left, find_right]
+    answer = 0
+    main()
+    print(answer)
+
+
+
+
+
