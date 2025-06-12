13c4acc0de7a3e84de463f1d9904d78446948d45
Minhee-Jang/장민희 <minhee_jang@ewha.ac.kr>
2025-06-12 17:34:46 +0900

    Title: 코드트리 빵, Time: 57ms, Memory: 16MB, Status: Passed - Codetree

--- 250612/코드트리 빵/codetree-mon-bread.py
+++ 250612/코드트리 빵/codetree-mon-bread.py
@@ -4,7 +4,7 @@ def inrange(r, c):
     return 0<=r<N and 0<=c<N
 
 def FindBase(er, ec, cant, p):  # occupy 상관없음 -> 순간이동할거임
-    global now
+    global now, occupy
     # 행, 열이 작은 순서대로 고르기
     q = deque([(er, ec)])
     v = [[False for _ in range(N)] for _ in range(N)]
@@ -18,12 +18,12 @@ def FindBase(er, ec, cant, p):  # occupy 상관없음 -> 순간이동할거임
         for dr, dc in D:
             nr, nc = r + dr, c + dc
 
-            if inrange(nr, nc) and not v[nr][nc]: # 방문 안한 곳
+            if inrange(nr, nc) and not v[nr][nc] and not occupy[nr][nc]: # 방문 안한 곳
                 tmp[nr][nc] = min(tmp[nr][nc], tmp[r][c] + 1)
                 v[nr][nc] = True
                 q.append((nr, nc))
 
-                if game[nr][nc] == 1 and not occupy[nr][nc]:
+                if game[nr][nc] == 1:
                     tmpDist = (tmp[nr][nc], nr, nc)
                     if minDist > tmpDist: # 최단거리
                         minDist = tmpDist # 업데이트
@@ -36,10 +36,12 @@ def Go(turn, cant):
     global now, Done
 
     for i in range(1, turn+1): #움직일 대상.
-        if i in Done: continue
+        if visited[i]: continue
         q = deque([(now[i][0], now[i][1], [])])  # 현재 위치/ 이동 경로
         v = [[False for _ in range(N)] for _ in range(N)]
         v[now[i][0]][now[i][1]] = True
+        # 이동해야하는 사람 BFS
+        # 상좌우하 4방향
         flag = True
 
         while flag:
@@ -52,10 +54,12 @@ def Go(turn, cant):
                 # 방문 가능
                 if nr == want[i][0] and nc == want[i][1]: # 편의점 도착
                     route.append((r, c))
+
                     if len(route) == 1:
                         cant.append((nr, nc))
-                        Done.append(i)
+                        visited[i] = True
                         now[i] = (nr, nc)
+                        Done.append(i)
                         flag = False
                         break
                     else:
@@ -96,22 +100,27 @@ if __name__=="__main__":
     minute = 0  # 시간 최대로 걸릴려면 N*N인가 ...
     while len(Done) != M: # 모두가 완료할때까지
         minute += 1
-
         cant = []
         if minute ==1:
             er, ec = want[minute]
             cant = FindBase(er, ec, cant, minute)
+            for a, b in cant:
+                occupy[a][b] = True
         else:
             if minute<=M:  # T는 베이스캠프 탐색
                 # 편의점을 향해 이동 -> 현재가 T분이라면 1 ~ T-1 사람만 탐색 (N*N*M
                 cant = Go(minute-1, cant)
+                for a, b in cant:
+                    occupy[a][b] = True
                 # 베이스캠프 찾기
                 er, ec = want[minute]
                 cant = FindBase(er, ec, cant, minute)  # 현재 위치
+                for a, b in cant:
+                    occupy[a][b] = True
             else:
                 cant = Go(M, cant)
-        # 이동 불가한 칸 체크  -> cant
-        for a, b in cant:
-            occupy[a][b] = True
+                # 이동 불가한 칸 체크  -> cant
+                for a, b in cant:
+                    occupy[a][b] = True
 
     print(minute)
