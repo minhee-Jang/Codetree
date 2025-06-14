4081e7d9e7c547bb56dcc7f3a731e7605f016612
Minhee-Jang/장민희 <minhee_jang@ewha.ac.kr>
2025-06-14 17:24:03 +0900

    Title: 미생물 연구, Time: 268ms, Memory: 24MB, Status: Passed - Codetree

--- 250614/미생물 연구/microbial-research.py
+++ 250614/미생물 연구/microbial-research.py
@@ -21,39 +21,38 @@ def moveMicro(prior, loc, t):
     # 살아있는 애들로만 배양용기 이동 -> 새로운 배양용기
     put = {i:False for i in range(1, Q+1)}
 
-    n=0
-    while n<len(prior):
-        if not alive[prior[n][1]]: continue
-        minr, minc, maxr, maxc = minmax(prior[n][2]) # 최대최소
-        dr, dc = maxr - minr, maxc - minc
-        # 해당 newEX 짤라서 -> 합이 0인지 확인 -> 맞으면 붙이고 아니면 이동
-        for c in range(N): # c가 작을수록
+    for i in range(len(prior)):
 
-             if put[prior[n][1]] == True: break
+        if not alive[prior[i][1]]: continue
+        minr, minc, maxr, maxc = minmax(prior[i][2]) # 최대최소
+        dr, dc = maxr - minr, maxc - minc
+
+        possible = False
+        for c in range(N): # c가 작을수록
              for r in range(N): # r이 작을수록
-                possible = True
-                if newEx[r][c] != 0:
-                    continue
-                if in_range(r+dr, c+dc):
+                if in_range(r+dr, c+dc): # 자르기 가능
                     # 검사하고 붙이기  ###########
-                    for i in range(r, r+dr+1):
-                        for j in range(c, c+dc+1): #
-                            if newEx[i][j] != 0:
-                                possible = False
-                                break
-                        if not possible: break
+                    br, bc = r - minr, c - minc
+                    for a, b in loc[prior[i][1]][2]:
+                        tr, tc = a +br, b +bc
+                        if newEx[tr][tc]!=0:
+                            possible = False
+                            break
+                        else:
+                            possible = True
                     # 좌표 계산
                     if possible:
-                        put[prior[n][1]] = True
+                        put[prior[i][1]] = True
                         br, bc = r - minr, c - minc
                         newR = []
-                        for a, b in loc[prior[n][1]][2]:
+                        for a, b in loc[prior[i][1]][2]:
                             nbr, nbc = a + br, b + bc # 블럭 위치
-                            newEx[nbr][nbc] = prior[n][1]
+                            newEx[nbr][nbc] = prior[i][1]
                             newR.append((nbr, nbc))
-                        loc[prior[n][1]][2] = newR # 좌표 교체
+                        loc[prior[i][1]][2] = newR # 좌표 교체
                         break
-        n+=1
+             if possible:
+                break
 
     for n in range(len(prior)):  # 죽은애들
         if not put[prior[n][1]]:
@@ -68,34 +67,37 @@ def result(ex):
     ans = 0
     q = deque([(0,0)])
     visited = [[False for _ in range(N)] for _ in range(N)]
-
+    visited[0][0] = True
     meet = []
 
     while q:
         r, c = q.popleft()
-        visited[r][c] = True
         for dr, dc in D:
             nr, nc = r + dr, c + dc
-            if in_range(nr, nc) and not visited[nr][nc]:
-                q.append((nr, nc))
-                #visited[nr][nc] = True
-                #print(ex[r][c], ex[nr][nc])
-
+            if in_range(nr, nc):
+                #print(meet, (ex[r][c], ex[nr][nc]))
                 if ex[r][c] != ex[nr][nc] and ex[r][c] !=0 and ex[nr][nc] != 0: # 둘이 다른 종이
+
                     if (ex[r][c], ex[nr][nc]) not in meet and (ex[nr][nc], ex[r][c]) not in meet:
                         meet.append((ex[r][c], ex[nr][nc]))
+                        #print('append')
+                if not visited[nr][nc]:
+                    q.append((nr,nc))
+                    visited[nr][nc] = True
+
     for a, b in meet:
         ans += loc[a][0]*loc[b][0]
+
     print(ans)
-    
     return
+
 
 def putMicro(ex, micro, n, loc):  # 배양용기는 moveMicro에서 시작됨
     global alive
 
     r1, c1, r2, c2 = micro
     for i in range(r1, r2+1):
-        for j in range(c1, c2+1): 
+        for j in range(c1, c2+1):
             ex[i][j] = n # 현재 미생물
 
 
@@ -129,17 +131,19 @@ def putMicro(ex, micro, n, loc):  # 배양용기는 moveMicro에서 시작됨
                             loc[m][2].append((nr, nc))
                             cnt +=1
                 loc[m][0] = cnt
-    # Loc 업데이트, Kill 진행
+
     for i in kill:
         for kr, kc in loc[i][2]:
             ex[kr][kc] = 0
         alive[i] = False
         loc[i][2] = []
+        loc[i][0] = 0
 
     for i in range(1, n+1):
         if not find[i]:
             alive[i] = False
             loc[i][2] = []
+            loc[i][0] = 0
 
     return ex, loc
 
@@ -164,7 +168,4 @@ if __name__=="__main__":
                 tmp.append(loc[j])
         prior = sorted(tmp, key=lambda x: (-x[0],x[1]))
         ex, loc = moveMicro(prior, loc, i)
-
         result(ex)
-
-
